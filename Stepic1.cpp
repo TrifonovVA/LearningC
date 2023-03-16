6fae72d28e183562ea7c3e8461b59fe70e7b54f5
TrifonovVA <TrifonovVladimirA@yandex.ru>
2023-03-16 14:24:49 +0300

    Stepic+SimpleCode

---
+++ Stepic1.cpp
@@ -0,0 +1,54 @@
+//1.5.14 Цикл while
+/* 
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n,n1,n2;
+    cin>>n2;
+    cin>>n1;
+    cin>>n;
+    int lloc=0;
+    int lmin=0;
+    int max=0;
+    while (n!=0)
+    {
+        if(n1>n2 && n1>n)
+        {
+            if (lloc!=0)
+            {
+                if(lmin!=0)
+                {
+                    if (lmin>lloc)
+                    {
+                        lmin=lloc;
+                    }
+                }
+                else
+                {
+                    lmin=lloc;
+                }
+            }
+            lloc=1;
+            
+        }
+        else
+        {
+            if (lloc!=0)
+            {
+                lloc++;
+            }
+        }
+        n2=n1;
+        n1=n;
+        cin>>n;
+    }
+
+
+    cout<<lmin;
+    return 0;
+} */
+
+
+//1.6.1 
